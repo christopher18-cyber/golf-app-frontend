b6bbea58ee25e09b6048438aa1d3caf83f117f23
christopher18-cyber <christopheradegoke4@gmail.com>
2026-03-26 07:42:57 +0100

    fixed the link in the dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import {
 	getMyScoresApi,
 	addScoreApi,
